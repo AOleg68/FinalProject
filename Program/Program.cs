df9bc4869f3f37e61995070bb781dba6bfdc66e0
Oleg <o.antonenko2011@yandex.ru>
2022-11-22 13:02:04 +0300

    Решение задачи + исправление Блок-Схемы

--- Program/Program.cs
+++ Program/Program.cs
@@ -3,3 +3,42 @@
 // либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+
+Console.Write("Количество элементов массива: ");
+int Length = Convert.ToInt32(Console.ReadLine());
+
+string[] arrayin = new string[Length];
+for (int i = 0; i < Length; i++)
+{
+    Console.WriteLine($"Введите {i+1}-й элемент: ");
+    string element = Convert.ToString(Console.ReadLine());
+    arrayin[i] = element;
+}
+
+string[] arrayout = new string[Length];
+int n = 3;
+int pos = 0;
+
+for (int j = 0; j < Length; j++)
+{
+    if (arrayin[j].Length <= n)
+    {
+        arrayout[pos] = arrayin[j];
+        pos++;
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("Элементы массива <= 3 символам:");
+PrintArray(arrayout);
